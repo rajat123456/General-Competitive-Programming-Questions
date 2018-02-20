12d4eb7819640eeeffe716b2700108f2db8fb941
Rajat Rawat <rajatrawat1997@gmail.com>
2018-02-20 13:32:48 +0530

    Create  and % operators.java

---
+++ Division and Remainder without using/and % operators.java
@@ -0,0 +1,79 @@
+//Division and Remainder without using / and % operators 
+
+import java.util.*;
+public class Main
+{
+    static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
+	    int d=sc.nextInt();
+	    for(int i=0;i<d;i++)
+	     {
+	         int a=sc.nextInt();
+		     int b=sc.nextInt();
+	         quo(a,b);
+	         rem(a,b);
+	     }
+	     
+		}
+		static void quo(int a, int b)
+		 {
+		     
+		     int total=0;
+		     int divisor=b;
+		     int divident=a;
+		     if(a==0 && b==0)
+		     {
+		     System.out.println("Undefined");
+		     return;
+		     }
+		     else if(b>a)
+		     {
+		     System.out.println("Quotient is 0");
+		     return;
+		     }
+		     int i;
+		     for(i=1;i<=a;i++)
+		      {
+		          total=divisor*i;
+		          if(total>divident)
+		          {
+		          System.out.println("Quotient is "+(i-1));
+		          break;
+		          }
+		          else if(total==divident)
+		          {
+		          System.out.println("Quotient is "+i);
+		          break;
+		          }
+		     
+		      }
+		      return;
+		           
+		 }
+		 
+		 static void rem(int a, int b)
+		 {
+		     int divisor=b;
+		     int divident=a;
+		     int k=a;
+		     if(a==0 && b==0)
+		     {
+		      System.out.println("Undefined");
+		      return;
+		     }
+		      else if(b>a)
+		     {
+		     System.out.println("Remainder is "+a);
+		     return;
+		     }
+		     else
+		     {
+		         while(k>=b)
+		       {
+		        k=k-b;
+		       }
+		     }
+		      System.out.println("Remainder is "+k);
+		     
+		 }
+}
