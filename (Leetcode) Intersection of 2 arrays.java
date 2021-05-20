7efab0c1315b36b7134c7dfdaea086ed9f074839
Rajat Rawat <rajatrawat1997@gmail.com>
2021-05-21 01:38:09 +0530

    Update (Leetcode) Intersection of 2 arrays.java

--- (Leetcode) Intersection of 2 arrays.java
+++ (Leetcode) Intersection of 2 arrays.java
@@ -1,9 +1,9 @@
 // Problem Link: https://leetcode.com/problems/intersection-of-two-arrays/submissions/
 
+
+// Approach 1: By Sorting both array
 // Time Complexity: O(nlogn) because of sorting
 // Space Complexity: O(1)
-
---------------------------------------------------
 
 import java.util.*;
 class Solution {
@@ -40,6 +40,60 @@ class Solution {
             else
             ++p1;
             
+        }
+        
+        //Since answer req in array, so converting arraylist to array
+        int[]res=new int[al.size()];
+        
+        
+        //copying arraylist elemenst to array
+        for(int g=0;g<al.size();g++)
+        {
+            res[g]=al.get(g);
+        }
+        
+        //return resultant array
+        return res;
+    }
+        
+}
+
+
+
+
+-----------------------------------------------------
+    
+    
+// Approach 2: Using Additional Array
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+    
+import java.util.*;
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        
+        //because max array size can be 1000 given in ques
+        int globalarray[]=new int[1001];
+        
+        ArrayList<Integer>al=new ArrayList<>();
+    
+        // First checking that number in array 1
+        for(int q=0; q<nums1.length; q++)
+        {
+            if(globalarray[nums1[q]]==0)
+                globalarray[nums1[q]]=1;
+        }
+        
+        // If that number also exist in array 2, take it in our answer
+        for(int w=0; w<nums2.length; w++)
+        {
+            if(globalarray[nums2[w]]==1)
+            {
+                al.add(nums2[w]);
+                
+                //setting that matched element to 0, to avoid duplicates
+                globalarray[nums2[w]]=0;
+            }
         }
         
         //Since answer req in array, so converting arraylist to array
